d106421e9ae1e904b0eeec67253a054f4eedca3b
Salvador Oliveira <slsalvadorlucas@gmail.com>
2021-12-03 14:54:07 -0400

    Add testing for the dropdown opening of the search

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,4 +1,23 @@
-it("Show autocomplete", () => {
+const waitFor = (selector) => {
+  return new Promise((resolve, reject) => {
+    const intervalId = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(intervalId);
+        clearTimeout(timeoutId);
+        resolve();
+      }
+    }, 100);
+
+    const timeoutId = setTimeout(() => {
+      clearInterval(intervalId);
+      reject();
+    }, 2000);
+  });
+};
+
+beforeEach(() => {
+  document.querySelector("#target").innerHTML = "";
+
   createAutoComplete({
     root: document.querySelector("#target"),
     fetchData() {
@@ -12,8 +31,23 @@ it("Show autocomplete", () => {
       return movie.Title;
     },
   });
+});
 
+it("Dropdown starts closed", () => {
   const dropdown = document.querySelector(".dropdown");
 
   expect(dropdown.className).not.to.include("is-active");
+});
+
+it("After searching, dropdown open up", async () => {
+  const input = document.querySelector("input");
+
+  input.value = "Justice League";
+
+  input.dispatchEvent(new Event("input"));
+
+  await waitFor(".dropdown-item");
+
+  const dropdown = document.querySelector(".dropdown");
+  expect(dropdown.className).to.include("is-active");
 });
